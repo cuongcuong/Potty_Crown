a1d4b60caf2f9e22fed860fd7ac2200ee2cba565
cuongcuong <dfg242@yahoo.com>
2015-06-15 15:46:05 +0700

    test
    test

---
+++ PauseScreen.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var PauseCanvas : Canvas;
+
+function Start () {
+
+}
+
+function Update ()
+{
+	if (Input.GetKeyDown(KeyCode.Escape))
+	{
+		PauseCanvas.enabled = true;
+		Time.timeScale = 0;
+		Screen.lockCursor = false;
+		Cursor.visible = true;
+	}
+}
+
+function ResumeGame () 
+{
+	PauseCanvas.enabled = false;
+		Time.timeScale = 1;
+		Screen.lockCursor = true;
+		Cursor.visible = false;
+
+}
